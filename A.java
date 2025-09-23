5c55c6024875a6a5eb4d3bc7a814824e65e9b7a9
DamasoRafael <rafael.rpdamaso@gmail.com>
2025-09-22 22:35:08 -0300

    alteração na classe A fork colega

---
+++ A.java
@@ -0,0 +1,26 @@
+public class A {
+    private int A1;
+    private float A2;
+
+    public int getA1() { return A1; }
+    public void setA1(int A1) { this.A1 = A1; }
+
+    public float getA2() { return A2; }
+    public void setA2(float A2) { this.A2 = A2; }
+
+    public void MA1() {
+        System.out.println("Método MA1 executado");
+    }
+
+    public void MA2() {
+        System.out.println("Método MA2 executado");
+    }
+    public void MA3() {
+    System.out.println("Alteração a classe A a partir do clone");
+}
+
+    public int getSoma(int a, int b) {
+    return a + b;
+}
+
+}
